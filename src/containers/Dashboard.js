5eb89c386fd3a03cafcf5f5737f7edf97653601e
Cédric Royer <cedricroyerpro@gmail.com>
2024-12-15 01:23:52 +0100

    Fix multiple event listeners by removing existing 'click' handlers before adding new ones

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -145,7 +145,7 @@ handleShowTickets(e, bills, index) {
       this.counter ++
     }
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`).off('click').on('click', (e) => this.handleEditTicket(e, bill, bills))
     })
     return bills
   }
